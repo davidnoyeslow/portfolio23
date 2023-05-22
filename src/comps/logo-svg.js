641c2035f99ffea8e55b8724fceaeab1cd408336
David Low <david.noyes.low@gmail.com>
2023-05-22 09:55:58 -0400

    desesign of homepage and logo

---
+++ src/comps/logo-svg.js
@@ -0,0 +1,325 @@
+import React, {useEffect} from "react";
+import { motion, useAnimationControls } from 'framer-motion';
+
+function Logo() {
+    // const outline = useAnimationControls();
+
+    // useEffect(() => {
+        
+    //     outline.start({
+          
+    //       rotate: [0, 360],
+          
+    //       transition: {
+    //         duration: 5,
+    //         repeat: Infinity,
+    //         repeatType:"reverse",
+            
+    //         }
+          
+    //     })
+    //   }, []);
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="218.641"
+      height="212"
+      version="1.1"
+      viewBox="0 0 55.732 56.092"
+      className="logo"
+    >
+      <defs>
+        <marker
+          id="Dot"
+          markerHeight="9"
+          markerWidth="9"
+          orient="auto"
+          overflow="visible"
+          preserveAspectRatio="xMidYMid"
+          refX="0"
+          refY="0"
+          viewBox="-1 -5 4.667 15.667"
+    
+        >
+          <path
+            fill="context-stroke"
+            fillRule="evenodd"
+            stroke="context-stroke"
+            strokeWidth=".5pt"
+            d="M2.5 0a2.5 2.5 0 01-5 0c0-1.38 1.15-2.5 2.5-2.5A2.5 2.5 0 012.5 0z"
+          ></path>
+        </marker>
+        <marker
+          id="3dots"
+          markerHeight="9.5"
+          markerWidth="7.5"
+          orient="auto-start-reverse"
+          overflow="visible"
+          preserveAspectRatio="xMidYMid"
+          refX="0"
+          refY="0"
+          viewBox="0 0 8 5"
+        >
+          <g
+            fill="context-stroke"
+            stroke="context-stroke"
+            transform="scale(.5)"
+          >
+            <circle cx="4" cy="0" r="1"></circle>
+            <circle cx="8" cy="0" r="1"></circle>
+            <circle cx="12" cy="0" r="1"></circle>
+          </g>
+        </marker>
+        <clipPath id="clipPath33978-1" clipPathUnits="userSpaceOnUse">
+          <ellipse
+            cx="90.113"
+            cy="122.252"
+            fill="#000"
+            fillOpacity="1"
+            stroke="none"
+            strokeDasharray="none"
+            strokeDashoffset="0"
+            strokeOpacity="1"
+            strokeWidth="0.192"
+            paintOrder="fill markers stroke"
+            rx="2.239"
+            ry="2.047"
+          ></ellipse>
+        </clipPath>
+        <clipPath id="clipPath33974-8" clipPathUnits="userSpaceOnUse">
+          <ellipse
+            cx="89.988"
+            cy="122.749"
+            fill="#000"
+            fillOpacity="1"
+            stroke="none"
+            strokeDasharray="none"
+            strokeDashoffset="0"
+            strokeOpacity="1"
+            strokeWidth="0.233"
+            paintOrder="fill markers stroke"
+            rx="2.657"
+            ry="2.524"
+          ></ellipse>
+        </clipPath>
+        <filter
+          id="filter33944-1"
+          width="3.297"
+          height="3.513"
+          x="-1.149"
+          y="-1.256"
+          colorInterpolationFilters="sRGB"
+        >
+          <feGaussianBlur stdDeviation="0.751"></feGaussianBlur>
+        </filter>
+        <clipPath id="clipPath33970-0" clipPathUnits="userSpaceOnUse">
+          <ellipse
+            cx="90.113"
+            cy="122.252"
+            fill="#000"
+            fillOpacity="1"
+            stroke="none"
+            strokeDasharray="none"
+            strokeDashoffset="0"
+            strokeOpacity="1"
+            strokeWidth="0.192"
+            paintOrder="fill markers stroke"
+            rx="2.239"
+            ry="2.047"
+          ></ellipse>
+        </clipPath>
+        <filter
+          id="filter33948-8"
+          width="2.057"
+          height="2.154"
+          x="-0.528"
+          y="-0.577"
+          colorInterpolationFilters="sRGB"
+        >
+          <feGaussianBlur stdDeviation="0.932"></feGaussianBlur>
+        </filter>
+        <clipPath id="clipPath33966-7" clipPathUnits="userSpaceOnUse">
+          <ellipse
+            cx="90.113"
+            cy="122.252"
+            fill="#000"
+            fillOpacity="1"
+            stroke="none"
+            strokeDasharray="none"
+            strokeDashoffset="0"
+            strokeOpacity="1"
+            strokeWidth="0.192"
+            paintOrder="fill markers stroke"
+            rx="2.239"
+            ry="2.047"
+          ></ellipse>
+        </clipPath>
+        <filter
+          id="filter33952-1"
+          width="2.05"
+          height="2.132"
+          x="-0.525"
+          y="-0.566"
+          colorInterpolationFilters="sRGB"
+        >
+          <feGaussianBlur stdDeviation="0.834"></feGaussianBlur>
+        </filter>
+        <clipPath id="clipPath33962-4" clipPathUnits="userSpaceOnUse">
+          <ellipse
+            cx="88.982"
+            cy="123.254"
+            fill="#000"
+            fillOpacity="1"
+            stroke="none"
+            strokeDasharray="none"
+            strokeDashoffset="0"
+            strokeOpacity="1"
+            strokeWidth="0.118"
+            paintOrder="fill markers stroke"
+            rx="1.348"
+            ry="1.273"
+          ></ellipse>
+        </clipPath>
+        <filter
+          id="filter33956-7"
+          width="1.411"
+          height="1.452"
+          x="-0.206"
+          y="-0.226"
+          colorInterpolationFilters="sRGB"
+        >
+          <feGaussianBlur stdDeviation="0.251"></feGaussianBlur>
+        </filter>
+      </defs>
+      <g transform="translate(-87.297 -132.435)">
+        <g transform="matrix(1.99368 0 0 1.98377 -89.375 -136.006)">
+          <path
+            fill="none"
+            fillOpacity="1"
+            stroke="#000"
+            strokeDasharray="none"
+            strokeOpacity="1"
+            strokeWidth="0.3"
+            d="M90.23 152.414a12.442 12.758 0 014.036-12.39l8.118 9.667z"
+            paintOrder="stroke markers fill"
+          ></path>
+          <path
+            fill="none"
+            stroke="#000"
+            strokeDasharray="none"
+            strokeDashoffset="0"
+            strokeWidth="0.527"
+            markerStart="url(#Dot)"
+            d="M115.869 152.582a13.67 13.874 0 01-11.428 10.615 13.67 13.874 0 01-13.856-7.03 13.67 13.874 0 011.508-15.648 13.67 13.874 0 0114.937-4.17"
+            paintOrder="stroke markers fill"
+          ></path>
+          <path
+            fill="none"
+            stroke="#000"
+            strokeDasharray="none"
+            strokeWidth="0.4"
+            d="M98.148 140.097a10.468 10.469 0 0113.77 4.788 10.468 10.469 0 01-4.37 13.91 10.468 10.469 0 01-14.034-3.951"
+            paintOrder="stroke markers fill"
+          ></path>
+          <path
+            fill="none"
+            stroke="#000"
+            strokeDasharray="none"
+            strokeDashoffset="0"
+            strokeWidth="0.3"
+            markerStart="url(#3dots)"
+            d="M110.309 148.526a7.83 7.86 0 01-5.532 8.589 7.83 7.86 0 01-9.303-4.17 7.83 7.86 0 012.677-9.868"
+            paintOrder="stroke markers fill"
+          ></path>
+          <path
+            fill="none"
+            stroke="#000"
+            strokeDasharray="none"
+            strokeWidth="0.224"
+            d="M103.677 155.383a5.84 5.898 0 01-6.402-3.254 5.84 5.898 0 011.542-7.07 5.84 5.898 0 017.163-.237"
+            paintOrder="stroke markers fill"
+          ></path>
+          <path
+            fill="none"
+            stroke="#000"
+            strokeDasharray="none"
+            strokeWidth="0.169"
+            d="M101.577 145.274a4.397 4.46 0 014.871 2.286 4.397 4.46 0 01-.938 5.362 4.397 4.46 0 01-5.35.446"
+            paintOrder="stroke markers fill"
+          ></path>
+          <g transform="matrix(.2216 0 0 .2216 81.103 121.152)">
+            <path
+              style={{ mixBlendMode: "normal" }}
+              fill="#333"
+              fillOpacity="1"
+              stroke="none"
+              strokeDasharray="none"
+              strokeDashoffset="0"
+              strokeOpacity="1"
+              strokeWidth="0.1"
+              d="M89.286 120.35c-.618.225-1.095.69-1.301 1.268l2.717 2.608c.702-.175 1.267-.651 1.515-1.277-1.232-.569-2.324-1.301-2.931-2.6z"
+              clipPath="url(#clipPath33970-0)"
+              filter="url(#filter33948-8)"
+              paintOrder="fill markers stroke"
+              transform="matrix(3.93395 0 0 4.07263 -257.818 -369.343)"
+            ></path>
+            <g transform="matrix(1.01556 0 0 1 -1.367 0)">
+              <g
+                fillOpacity="1"
+                stroke="none"
+                strokeDasharray="none"
+                strokeDashoffset="0"
+                strokeOpacity="1"
+                paintOrder="fill markers stroke"
+                transform="matrix(1 0 0 1.01842 0 -2.522)"
+              >
+                <ellipse
+                  cx="90.113"
+                  cy="122.252"
+                  fill="#000"
+                  strokeWidth="0.192"
+                  clipPath="url(#clipPath33978-1)"
+                  rx="2.239"
+                  ry="2.047"
+                  transform="matrix(3.93395 0 0 4.07263 -257.818 -369.343)"
+                ></ellipse>
+                <ellipse
+                  style={{ mixBlendMode: "normal" }}
+                  cx="90.95"
+                  cy="121.444"
+                  fill="#fff"
+                  strokeWidth="0.067"
+                  clipPath="url(#clipPath33974-8)"
+                  filter="url(#filter33944-1)"
+                  rx="0.785"
+                  ry="0.718"
+                  transform="matrix(3.31548 0 0 3.3034 -201.671 -276.947)"
+                ></ellipse>
+                <path
+                  style={{ mixBlendMode: "normal" }}
+                  fill="#1a1a1a"
+                  strokeWidth="0.1"
+                  d="M88.578 120.762a1.98 1.98 0 00-.699 1.367l2.265 2.17a2.35 2.35 0 001.55-.598c-1.322-.655-2.49-1.485-3.116-2.939z"
+                  clipPath="url(#clipPath33966-7)"
+                  filter="url(#filter33952-1)"
+                  transform="matrix(3.93395 0 0 4.07263 -257.818 -369.343)"
+                ></path>
+                <path
+                  style={{ mixBlendMode: "normal" }}
+                  fill="#000"
+                  strokeWidth="0.1"
+                  d="M87.98 121.633c-.07.2-.106.409-.106.619 0 1.13 1.003 2.047 2.24 2.047.235 0 .468-.034.692-.101-1.334-.519-2.237-1.407-2.827-2.565z"
+                  clipPath="url(#clipPath33962-4)"
+                  filter="url(#filter33956-7)"
+                  transform="matrix(6.53455 0 0 6.549 -484.775 -678.648)"
+                ></path>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default Logo;
